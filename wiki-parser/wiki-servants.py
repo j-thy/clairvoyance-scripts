b2b9e81102e191d523ba3c21927fb5bf936170cf
jjting <jting@vols.utk.edu>
2023-08-26 14:04:30 -0400

    Parse servant IDs

--- wiki-parser/wiki-servants.py
+++ wiki-parser/wiki-servants.py
@@ -7,15 +7,29 @@ def parse(category_name):
     FILE_NAMES = []
     site = pywikibot.Site()
     category = pywikibot.Category(site, category_name)
-    count = 0
+    done = 0
     for page in category.articles():
+        done = 0
         title = page.title()
-        print(title)
+        # print(title)
         text = page.text
         # print(text)
         wikicode = mwparserfromhell.parse(text)
         filtered = wikicode.filter_templates()
-        print(filtered[1].get("id").value)
-        break
+        # print(f'{filtered[1].get("id").value.strip()}: {title}')
+        for template in filtered:
+            if template.name.strip() == 'CharactersNew':
+                done = 1
+                try:
+                    id_val = template.get("id").value.strip()
+                    if id_val.isdigit():
+                        print(f'{template.get("id").value.strip()}: {title}')
+                    else:
+                        print(f' - No ID found for {title}')
+                except ValueError:
+                    print(f' - No ID found for {title}')
+                break
+        if done == 0:
+            print(f' - No template found for {title}')
 
 parse(CATEGORY)
